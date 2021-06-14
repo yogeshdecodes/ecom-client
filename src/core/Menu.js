6857d454ade845fcb2a9f78a03787aa344a32f5e
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-06-15 00:23:42 +0530

    Adding styling to menu 🎨

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,42 +1,73 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-const Menu = () => {
+const activeTab = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: '#2ecc72' };
+  } else {
+    return { color: '#ffffff' };
+  }
+};
+const Menu = ({ history }) => {
   return (
     <div>
       <ul className="nav nav-tabs bg-dark">
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link style={activeTab(history, '/')} className="nav-link" to="/">
             Home
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link
+            style={activeTab(history, '/cart')}
+            className="nav-link"
+            to="/cart"
+          >
             Cart
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link
+            style={activeTab(history, '/user/dashboard')}
+            className="nav-link"
+            to="/user/dashboard"
+          >
             Dasboard
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link
+            style={activeTab(history, '/admin/dashboard')}
+            className="nav-link"
+            to="/admin/dashboard"
+          >
             Admin Dashboard
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link
+            style={activeTab(history, '/signup')}
+            className="nav-link"
+            to="/signup"
+          >
             signup
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link
+            style={activeTab(history, '/signin')}
+            className="nav-link"
+            to="/signin"
+          >
             signin
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link
+            style={activeTab(history, '/signout')}
+            className="nav-link"
+            to="/signout"
+          >
             signout
           </Link>
         </li>
