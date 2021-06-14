af6babaccd2ac6a48b1d85d2023da06401c67084
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-06-15 01:07:26 +0530

    signup and signin form

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -2,9 +2,29 @@ import React from 'react';
 import Base from '../core/Base';
 
 const Signin = () => {
+  const signinForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <button className="btn-success btn-block">submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
   return (
     <Base title="signup page" description="A page for user signin">
       <h1>Signin page</h1>
+      {signinForm()}
     </Base>
   );
 };
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -2,9 +2,32 @@ import React from 'react';
 import Base from '../core/Base';
 
 const Signup = () => {
+  const signupForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input className="form-control" type="text" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <button className="btn-success btn-block">submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
   return (
     <Base title="signup page" description="A page for user signup">
-      <h1>Signup page</h1>
+      {signupForm()}
     </Base>
   );
 };
