50dac06c0ba15135c312480410bef31c8ca5b503
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-06-24 15:51:00 +0530

    feat: Admin delete product

--- src/Routes.js
+++ src/Routes.js
@@ -11,6 +11,7 @@ import AddCategory from './admin/AddCategory';
 import ManageCategories from './admin/ManageCategories';
 import AddProduct from './admin/AddProduct';
 import { updateProduct } from './admin/helper/adminapicall';
+import ManageProducts from './admin/ManageProducts';
 
 const Routes = () => {
   return (
@@ -33,7 +34,7 @@ const Routes = () => {
         />
         <AdminRoute path="/admin/create/product" exact component={AddProduct} />
 
-        {/* <AdminRoute path="/admin/products" exact component={ManageProduct} /> */}
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
         <AdminRoute
           path="/admin/product/update/:productId"
           exact
